fd5db5ce30ccee758e5f08405b6610ebbc5dae1a
Rudra Pratap Singh <rpsingh7672@gmail.com>
2026-01-15 00:47:11 +0530

    Create script.js

---
+++ JAVASCRIPT/Data Types Check/script.js
@@ -0,0 +1,12 @@
+function check(){
+    let name = "Rudra";
+    let age = 20;
+    let isStudent = true;
+    let city;
+    let phone = null;
+    console.log(typeof name);
+    console.log(typeof age);
+    console.log(typeof isStudent);
+    console.log(typeof city);
+    console.log(typeof phone);
+}
